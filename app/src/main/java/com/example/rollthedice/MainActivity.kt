f30f2a9e5f03950a78ceea1781d0ccb3c0f83bc5
samuelepierotti <samy.pierotti@gmail.com>
2025-04-24 11:31:58 +0200

    Completamento prima e seconda activity

--- app/src/main/java/com/example/rollthedice/MainActivity.kt
+++ app/src/main/java/com/example/rollthedice/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.rollthedice
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,8 +20,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        button = findViewById(R.id.button_lancia)
         button.setOnClickListener(View.OnClickListener {
-
+            var random = (1..6).random()
+            var toast = Toast.makeText(this, "Estratto numero $random", Toast.LENGTH_LONG)
+            toast.show()
+            var intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("random", random)
+            startActivity(intent)
 
         }
         )
--- app/src/main/java/com/example/rollthedice/SecondActivity.kt
+++ app/src/main/java/com/example/rollthedice/SecondActivity.kt
@@ -1,6 +1,7 @@
 package com.example.rollthedice
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +12,17 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_second)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        var random = intent.getIntExtra("random", -1)
+        var imageViewSecond = findViewById<ImageView>(R.id.imageViewSecond)
+        var resource = when(random) {
+            1 -> R.drawable.dice_face_1
+            2 -> R.drawable.dice_face_2
+            3 -> R.drawable.dice_face_3
+            4 -> R.drawable.dice_face_4
+            5 -> R.drawable.dice_face_5
+            6 -> R.drawable.dice_face_6
+            else -> {R.drawable.dices}
         }
+        imageViewSecond.setImageResource(resource)
     }
 }
